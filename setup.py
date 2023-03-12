7133c179889c16ca69a9ade80f73d4fff85453fc
Thomas Champion <thomas@thomaschampion.net>
2023-03-12 17:28:39 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cleanup of SF fire incident data and migration from CSV to database storage',
+    author='Thomas Champion',
+    license='MIT',
+)
